759d84f7f5d2e54b0ae57502d21c24f9f3e5e21d
Ankit <ankit.iiitbh@gmail.com>
2024-07-09 15:56:03 +0530

    feat: add database connection and update server setup Added db.js for database connection Update index.js to include database connection and added middleware for JSON parsing, URL encoding, and CORS.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./db");
+const cors = require("cors");
+
+// Load environment variables
+dotenv.config();
+
+// Initialize express application
 const app = express();
 
-const dotenv = require("dotenv");
-dotenv.config();
-const PORT = process.env.PORT;
+// Connect to the database
+connectDB();
 
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({ origin: true, credentials: true }));
+
+// PORT definition
+const PORT = process.env.PORT || 3000;
+
+// Start server
 app.listen(PORT, () => {
-  console.log(`App is listening on PORT: ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
